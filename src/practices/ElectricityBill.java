3948428d923e3d047bd3c2ea7e6bab3895f7ca6c
thom <hsbang.thom@gmail.com>
2022-05-20 01:46:44 +0900

    practices

---
+++ src/practices/ElectricityBill.java
@@ -0,0 +1,11 @@
+package practices;
+
+public class ElectricityBill {
+
+    private static final double UNIT_PRICE_BELOW_100kWh_INCLUDED = 60.7;
+    private static final double UNIT_PRICE_OVER_100kWh = 125.9;
+    private static final double UNIT_PRICE_OVER_200kWh = 187.9;
+    private static final double UNIT_PRICE_OVER_300kWh = 280.6;
+    private static final double UNIT_PRICE_OVER_400kWh = 417.7;
+    private static final double UNIT_PRICE_OVER_500kWh = 670.6;
+}
