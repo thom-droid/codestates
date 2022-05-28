5b4645428e59b03b7553e5de1e18329cc10d74d6
banghs <hsbang.thom@gmail.com>
2022-05-28 17:11:43 +0900

    problem working on

--- src/ds/queue_stack/PrinterPerformance.java
+++ src/ds/queue_stack/PrinterPerformance.java
@@ -2,6 +2,7 @@ package ds.queue_stack;
 
 import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.stream.Collectors;
 
 public class PrinterPerformance {
@@ -34,38 +35,79 @@ public class PrinterPerformance {
         return time;
     }
 
+
+    // lets say we have buffersize the size of 2 with the capacity of 10, and documents are [5,4,2,3,4]
+    // the first work for [5] will be done in 3 seconds from the insertion to being printed out.
+    // and it will take one second more with the next document [4], 3 + 1 = four seconds altogether,
+    // since there is enough space left for [4] after [5] is put onto the workbench.
+
+    // no work can be loaded anymore since workbench is full, and capacity available is small (10 - 5 - 4 = 1)
+    // so in this case next document have to wait until workbench is cleared, and it restores its capacity back
+
+
     private static int queuePrinter2(int bufferSize, int capacities, int[] documents) {
+
         // buffer 공간을 아예 queue로 만들자.
-        int time = bufferSize;
-        ArrayDeque<Integer> deque = new ArrayDeque<>(bufferSize);
+        int time = 0;
         int remainedCapacities = capacities;
-        // 넣고 사이즈가 꽉 찼거나 capa가 딸리면 큐에서 top을 제거
-        for (int i = 0; i < documents.length; i++) {
-            int node = documents[i];
+        boolean isStreak = true;
 
-            // 큐가 꽉 찼을 때
-            // 큐에서 제거하고 capa를 회복
-            if (deque.size() == bufferSize) {
-                remainedCapacities += deque.poll();
-            }
+        ArrayDeque<Integer> workbench = new ArrayDeque<>(bufferSize);
+        LinkedList<Integer> documentQueue = Arrays.stream(documents).boxed().collect(Collectors.toCollection(LinkedList::new));
 
-            if (remainedCapacities > node) {
-                deque.add(node);
-                remainedCapacities -= node;
-            }
-
-            // 남은 capa보다 노드가 크다면 capa가 돌아올 때가지 기다려야 함
-            
-
-
-            time++;
+        // when document is empty
+        if (documentQueue.isEmpty()) {
+            return 0;
         }
+
+        // iterate until documentQueue gets empty
+        while (documentQueue.size() > 0) {
+            int work = documentQueue.peek();
+
+            // if remained capacity is not enough or workbench is fully loaded
+            if (workbench.size() == bufferSize && !workbench.isEmpty()) {
+                remainedCapacities += workbench.poll();
+            }
+
+            // check whether there is enough space left for the next node
+            // and push the polled node to the workbench
+            // when work is being processed, capacity must be subtracted
+            // if there is enough capacity, it can make a streak, meaning that next insertion will only take 1 more second
+            if (remainedCapacities >= work) {
+                work = documentQueue.poll();
+                workbench.add(work);
+                remainedCapacities -= work;
+                time++;
+//                isStreak = true;
+            } else {
+                // if the next work needs the more capacities than left, then wait until the capacity has enough of it.
+                // but what if the next work needs the whole space to process, so it has to wait until all the work in the bench is cleared?
+                // we need to add only one buffer size time but in this code we add buffered size time every loop
+                remainedCapacities += workbench.poll();
+                isStreak = false;
+                continue;
+            }
+
+            if (!isStreak) {
+                time += bufferSize;
+            }
+
+            isStreak = true;
+
+
+            // i think i need to change the way of aproaching to this problem from the scratch .
+            // not adding the time at the end, but adding and subtracting the time when
+            // work is added and removed from the workbench..
+        }
+
+
         return time;
     }
 
     public static void main(String[] args) {
 
-        System.out.println(queuePrinter2(2, 10, new int[]{5, 5, 5, 5, 5}));
+        System.out.println(queuePrinter2(2, 12, new int[]{5,6,1,12,8,9,11}));
+        System.out.println(queuePrinter2(2, 10, new int[]{7, 4, 5, 6}));
     }
 }
 
