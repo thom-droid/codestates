bf616b5f1f93979e70277d38a28fea777807b01f
thom <hsbang.thom@gmail.com>
2022-06-07 10:50:49 +0900

    modified

--- src/daily/CalNumberInString.java
+++ src/daily/CalNumberInString.java
@@ -2,6 +2,7 @@ package daily;
 
 public class CalNumberInString {
     public static int numberSearch(String str) {
+        StringBuilder sb = new StringBuilder();
         int length = str.length();
         String regex = "[0-9]";
         int sum = 0;
@@ -9,16 +10,24 @@ public class CalNumberInString {
             String s = String.valueOf(str.charAt(i));
             if (s.matches(regex)) {
                 sum += Integer.parseInt(s);
+                continue;
             }
+            if (s.equals(" ")) {
+                continue;
+            }
+
+            sb.append(s);
         }
 
-        double result = (double) sum / length;
+        int newLength = sb.length();
+
+        double result = (double) sum / newLength;
 
         return (int) Math.round(result);
     }
 
     public static void main(String[] args) {
-//        System.out.println(numberSearch("Hello6 9World 2, Nic8e D7ay!"));
-        System.out.println(numberSearch("8ad"));
+        System.out.println(numberSearch("Hello6 9World 2, Nic8e D7ay!"));
+//        System.out.println(numberSearch("8ad"));
     }
 }
