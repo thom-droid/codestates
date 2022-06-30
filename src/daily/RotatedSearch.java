0b22f292da296deee0ba5d4b89aed7bd71b1910a
thom <hsbang.thom@gmail.com>
2022-06-30 10:41:44 +0900

    solved but by looking reference

--- src/daily/RotatedSearch.java
+++ src/daily/RotatedSearch.java
@@ -4,28 +4,41 @@ public class RotatedSearch {
 
     public static int rotatedArraySearch(int[] rotated, int target) {
 
-        // 이진 탐색을 하다 0을 만나면 0을 리턴
-        // 타겟보다 값이 크면 0의 왼편으로 넘어가서 검색
-        // 값이 작으면 0의 오른편에서 검색
-        return 0;
-    }
+        // 값이 변하는 구간을 찾자
+        // 이진 탐색을 하면서 0번지부터 n/2번지를 했을 때 오름차순이 아니라면
+        // 오름차순이 될 때까지 n/2 - 1 를 해나감
+        // 왼편의 가장 큰 값을 찾고 이걸 기준으로 target이랑 비교
 
-    private static int findZeroInRightHalf(int[] rotated) {
-        //find 0
-
-        int size = rotated.length;
         int left = 0;
-        int right = size - 1;
-        while (right < size && left < size) {
-            int pivot = (right + left) / 2;
+        int right = rotated.length - 1;
 
-            if (rotated[pivot] == 0) {
-                return pivot;
+        while (left <= right) {
+            int middle = (left + right) / 2;
+
+            if (rotated[middle] == target) {
+                return middle;
+            }
+            // {4, 5, 6, 0, 1, 2, 3}, 2)
+            if (rotated[left] < rotated[middle]) {
+
+                if (rotated[left] < target && target < rotated[middle]) {
+                    right = middle - 1;
+                } else {
+                    left = middle + 1;
+                }
+
+            } else {
+
+                if (rotated[right] > target && target > rotated[middle]) {
+                    left = middle + 1;
+                } else {
+                    right = middle - 1;
+                }
             }
 
-            right = pivot;
         }
-        
+
+
         return -1;
     }
 
