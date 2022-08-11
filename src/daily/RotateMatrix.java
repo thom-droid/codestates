584bd9c16ad5e4ce855549c62e9af7491f775e57
thom <hsbang.thom@gmail.com>
2022-08-12 00:08:59 +0900

    coplit : daily coding

--- src/daily/RotateMatrix.java
+++ src/daily/RotateMatrix.java
@@ -14,37 +14,68 @@ public class RotateMatrix {
                 {13, 14, 15, 16}
         };
 
+        int[][] matrix2 = new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9},
+                {10, 11, 12},
+                {13, 14, 15}
+        };
+
         int K = 1;
 
-        rotateMatrix(matrix, 1);
+//        Arrays.stream(rotateMatrix(matrix, 4)).forEach(r -> System.out.println(Arrays.toString(r)));
+        Arrays.stream(rotateMatrix(matrix2, 2)).forEach(r -> System.out.println(Arrays.toString(r)));
     }
 
     public static int[][] rotateMatrix(int[][] matrix, int K) {
         // TODO: K에 따라 M,N 바꿔주는 것, 예외 처리가 필요
-        int M = matrix[0].length;
-        int N = matrix.length;
 
+        if (matrix == null) {
+            return null;
+        }
+
+        if (K % 4 == 0) {
+            return matrix;
+        }
+
+        if (K > 4) {
+            K -= 4;
+        }
+
+        int M = matrix.length;
+        int N = matrix[0].length;
         int[][] rotated;
 
-        List<LinkedList<Integer>> stacks = new ArrayList<>();
+        for (int k = 0; k < K; k++) {
+            List<LinkedList<Integer>> stacks = new ArrayList<>();
 
-        for (int i = 0; i < M; i++) {
-            LinkedList<Integer> stack = new LinkedList<>();
-            for (int j = 0; j < N; j++) {
-                stack.push(matrix[j][i]);
+            for (int i = 0; i < N; i++) {
+                LinkedList<Integer> stack = new LinkedList<>();
+                for (int j = 0; j < M; j++) {
+                    stack.push(matrix[j][i]);
+                }
+                stacks.add(stack);
             }
-            stacks.add(stack);
-        }
 
-        rotated = new int[N][M];
+            int tmp = N;
+            N = M;
+            M = tmp;
 
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) {
-                rotated[i][j] = stacks.get(i).pop();
+            rotated = new int[M][N];
+
+            for (int i = 0; i < M; i++) {
+                for (int j = 0; j < N; j++) {
+                    rotated[i][j] = stacks.get(i).pop();
+                }
             }
-        }
 
-        Arrays.stream(rotated).forEach(x -> System.out.println(Arrays.toString(x)));
+            matrix = rotated;
+
+//            Arrays.stream(rotated).forEach(x -> System.out.println(Arrays.toString(x)));
+
+
+        }
 
         return matrix;
     }
